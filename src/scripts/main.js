650aaadae971c3a01ad017bbe179d728ab532fe6
AdelRomeo <11cage11@mail.ru>
2020-04-14 22:05:27 +0700

    popup v2

--- src/scripts/main.js
+++ src/scripts/main.js
@@ -1,11 +1,17 @@
-function f() {
-    let riskList = document.querySelectorAll('.btn-risk');
-    let popup = document.querySelector('.popup');
+(function f() {
+    let riskList = document.querySelectorAll('.btn-risk'); //список кнопок которые открывают popup
+    let popup = document.querySelector('.popup'); //popup
+    let closePopup = document.querySelector('.popup_close'); //кнопка закрывающая popup
 
     for (let i = 0; i < riskList.length -1; i++) {
         riskList[i].addEventListener('click', ()=>{
             popup.style.visibility = 'visible';
+            popup.style.opacity = '1';
         })
     }
-}
-f();
+
+    closePopup.addEventListener('click', ()=>{
+        popup.style.visibility = 'hidden';
+        popup.style.opacity = '0';
+    })
+})();
